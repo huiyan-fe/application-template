09915451075105188c9cb04c76011b5d285e7c37
Junior2Ran <hdr01@126.com>
2022-03-14 18:17:07 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,25 @@
-import {commonPlugins, commonEsbuild, commonCss} from '../../scripts/vite.common';
+import {commonPlugins, commonCss} from '../../scripts/vite.common';
 import {defineConfig} from 'vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [
-        ...commonPlugins,
-    ],
-    esbuild: {
-        ...commonEsbuild,
-    },
-    css: {
-        ...commonCss,
-    },
+export default defineConfig(({mode}) => {
+
+    const defineValue = {
+        'development': {
+            publicPath: '/',
+        },
+        'production': {
+            publicPath: '/',
+        },
+    };
+
+    return {
+        base: defineValue[mode].publicPath,
+        plugins: [
+            ...commonPlugins,
+        ],
+        css: {
+            ...commonCss,
+        },
+    };
 });
